2dc6b61dba33bed2e8fc9d46e0939c2a06f5bb2c
Avery Choke <averychokedev@gmail.com>
2022-02-12 15:19:40 +0800

    refactor: Remove firebase dependency and pure Web RTC room manager

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import firebase from 'firebase';
-import 'firebase/firestore';
+import React from 'react';
 import Routes from './Routes';
 
 const App: React.FunctionComponent = () => {
-    const [initialized, setInitialized] = useState(false);
-
-    useEffect(() => {
-        if (initialized) return;
-        const firebaseConfig = {
-            apiKey: 'AIzaSyChS4yqkRUzRhoVO0eiAgXe-5d5mXe5uPY',
-            authDomain: 'lami-game.firebaseapp.com',
-            projectId: 'lami-game',
-            storageBucket: 'lami-game.appspot.com',
-            messagingSenderId: '824137481818',
-            appId: '1:824137481818:web:6d799578f676a9fe4be263',
-            measurementId: 'G-VTLNW9P4T1',
-        };
-        firebase.initializeApp(firebaseConfig);
-        setInitialized(true);
-    }, [initialized]);
-
-    return initialized ? <Routes /> : null;
+    return <Routes />;
 };
 
 export default App;
